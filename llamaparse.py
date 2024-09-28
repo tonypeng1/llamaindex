6aa4edccdc22ada10f1eb501c55967cb72c3c0b3
Tony Peng <tony3t3t@hotmail.com>
2024-09-28 09:34:24 -0500

    First working code for text, table, and image

--- llamaparse.py
+++ llamaparse.py
@@ -8,14 +8,19 @@ from llama_index.core import (
                         VectorStoreIndex,
                         )
 from llama_index.core.node_parser import LlamaParseJsonNodeParser
+from llama_index.core.schema import ImageDocument, TextNode
 from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.llms.anthropic import Anthropic
 from llama_index.llms.mistralai import MistralAI
+from llama_index.llms.openai import OpenAI
+from llama_index.multi_modal_llms.anthropic import AnthropicMultiModal
 from llama_parse import LlamaParse
 
 from database_operation import (
                 check_if_milvus_database_collection_exist,
                 check_if_mongo_database_namespace_exist
                 )
+import openai
 
 from utility import (
                 get_article_link,
@@ -25,14 +30,9 @@ from utility import (
                 )                
 
 
-def load_document_llamaparse_jason(_article_link, _llama_cloud_api_key):
+def load_document_llamaparse_jason(_json_objs):
 
-    parser = LlamaParse(
-        api_key=_llama_cloud_api_key, 
-        verbose=True
-        )
-    json_objs = parser.get_json_result(_article_link)
-    json_list = json_objs[0]["pages"]
+    json_list = _json_objs[0]["pages"]
 
     documents = []
     for _, page in enumerate(json_list):
@@ -63,14 +63,12 @@ def get_nodes_from_document_llamaparse(
 
 
 def load_document_nodes_llamaparse(
-    _article_link,
+    _json_objs,
     _parse_method,
-    _llama_cloud_api_key,
     ):
     # Only load and parse document if either index or docstore not saved.
     _document = load_document_llamaparse_jason(
-                                        _article_link,
-                                        _llama_cloud_api_key
+                                        _json_objs
                                         )
     (_base_nodes,
      _object) = get_nodes_from_document_llamaparse(
@@ -80,40 +78,80 @@ def load_document_nodes_llamaparse(
     return _base_nodes, _object
 
 
-def create_and_save_llamaparse_vector_index_to_milvus_database(_nodes):
+def load_image_text_nodes_llamaparse(_json_objs):
 
-    deepcopy_nodes = deepcopy(_nodes)
+    anthropic_mm_llm = AnthropicMultiModal(max_tokens=1000,
+                                           model="claude-3-5-sonnet-20240620",
+                                           api_key=ANTHROPIC_API_KEY,
+                                           )
+    image_dicts = parser.get_images(_json_objs, 
+                                    download_path="./images/"
+                                    )
+    img_text_nodes = []
+    for image_dict in image_dicts:
+        image_doc = ImageDocument(image_path=image_dict["path"])
+        response = anthropic_mm_llm.complete(
+            prompt="Describe the image in detail.",
+            image_documents=[image_doc],
+        )
+        text_node = TextNode(text=str(response), 
+                             metadata={"path": image_dict["path"]}
+                             )
+        img_text_nodes.append(text_node)
 
-    # Remove metadata from nodes and objects before saving to vector store.
-    for node in deepcopy_nodes:
+    return img_text_nodes
+
+
+def create_and_save_llamaparse_vector_index_to_milvus_database(_base_nodes,
+                                                               _objects,
+                                                               _image_text_nodes):
+
+    deepcopy_base_nodes = deepcopy(_base_nodes)
+    deepcopy_objects = deepcopy(_objects)
+
+    # Remove metadata from base nodes and objects before saving to vector store (othrewise max character 
+    # in dynamic field in Milvus will be excceeded). Image text nodes have short metadata.
+    for node in deepcopy_base_nodes:
         node.metadata = {}
+
+    for obj in deepcopy_objects:
+        obj.metadata = {}
 
     # In llamaoparse for Uber 2022, save 274 nodes to vector store (200 text nodes, 
     # 74 index nodes),and also save to MongoDB with 74 index nodes. The removal of
     # metadata does not seem to impact the details of the 74 index nodes in MongoDB.
 
     _index = VectorStoreIndex(
-        nodes=deepcopy_nodes,
+        nodes=deepcopy_base_nodes + deepcopy_objects + _image_text_nodes,
         storage_context=storage_context_vector,
         )
+    
     return _index
 
 
 nest_asyncio.apply()
-llama_cloud_api_key = os.environ['LLAMA_CLOUD_API_KEY']
+LLAMA_CLOUD_API_KEY = os.environ['LLAMA_CLOUD_API_KEY']
+MISTRAL_API_KEY = os.environ['MISTRAL_API_KEY']
+ANTHROPIC_API_KEY = os.environ['ANTHROPIC_API_KEY']
+
+
 
 # Set OpenAI API key, LLM, and embedding model
-# openai.api_key = os.environ['OPENAI_API_KEY']
-# llm = OpenAI(model="gpt-3.5-turbo", temperature=0.0)
+openai.api_key = os.environ['OPENAI_API_KEY']
+llm = OpenAI(model="gpt-4o", temperature=0.0)
+Settings.llm = llm
+
+# llm = MistralAI(
+#     model="mistral-large-latest", 
+#     temperature=0.0,
+#     api_key=MISTRAL_API_KEY
+#     )
 # Settings.llm = llm
 
-mistral_api_key = os.environ['MISTRAL_API_KEY']
-llm = MistralAI(
-    model="mistral-large-latest", 
-    temperature=0.0,
-    api_key=mistral_api_key
-    )
-Settings.llm = llm
+# llm = Anthropic(model="claude-3-5-sonnet-20240620", 
+#                 api_key=ANTHROPIC_API_KEY,
+#                 temperature=0.0)
+# Settings.llm = llm
 
 # Create embedding model
 embed_model = OpenAIEmbedding(model_name="text-embedding-3-small")
@@ -122,8 +160,11 @@ embed_model_dim = 1536  # for text-embedding-3-small
 embed_model_name = "openai_embedding_3_small"
 
 # Create article link
-article_dictory = "uber"
-article_name = "uber_10q_march_2022.pdf"
+# article_dictory = "uber"
+# article_name = "uber_10q_march_2022.pdf"
+
+article_dictory = "attention"
+article_name = "attention_all.pdf"
 
 article_link = get_article_link(article_dictory,
                                 article_name
@@ -185,16 +226,27 @@ storage_context_summary = get_summary_storage_context(uri_mongo,
 
 # Load documnet nodes if either vector index or docstore not saved.
 if save_index_vector or add_document_vector or add_document_summary: 
+
+    parser = LlamaParse(
+                api_key=LLAMA_CLOUD_API_KEY, 
+                verbose=True
+                )
+    json_objs = parser.get_json_result(article_link)
+
+    image_text_nodes = load_image_text_nodes_llamaparse(json_objs)
+
     (base_nodes,
      objects) = load_document_nodes_llamaparse(
-                                            article_link,
+                                            json_objs,
                                             parse_method,
-                                            llama_cloud_api_key
                                             )
 
 if save_index_vector == True:
-    recursive_index = create_and_save_llamaparse_vector_index_to_milvus_database(base_nodes +
-                                                                                objects)
+    recursive_index = create_and_save_llamaparse_vector_index_to_milvus_database(
+                                                                    base_nodes,
+                                                                    objects, 
+                                                                    image_text_nodes
+                                                                    )
 
 else:
     # Load from Milvus database
@@ -204,15 +256,16 @@ else:
 
 if add_document_vector == True:
     # Save document nodes to Mongodb docstore at the server
-    storage_context_vector.docstore.add_documents(base_nodes + objects)
+    storage_context_vector.docstore.add_documents(base_nodes + objects + image_text_nodes)
 
 if add_document_summary == True:
     # Save document nodes to Mongodb docstore at the server
-    storage_context_summary.docstore.add_documents(base_nodes + objects)
+    storage_context_summary.docstore.add_documents(base_nodes + objects + image_text_nodes)
 
 recursive_query_engine = recursive_index.as_query_engine(
-    similarity_top_k=15, verbose=True
-)
+                                            similarity_top_k=15, 
+                                            verbose=True
+                                            )
 
 # query = "what is UBER Short-term insurance reserves reported in 2022?"
 # query = "what is UBER's common stock subject to repurchase in 2021?"
@@ -223,10 +276,18 @@ recursive_query_engine = recursive_index.as_query_engine(
 # query = "What is the free cash flow in 2021?"
 # query = "What is the gross bookings of delivery in Q3 2021?"
 # query = "What is the gross bookings in 2022?"
-query = "What is the value of mobility adjusted EBITDA in 2022?" 
+# query = "What is the value of mobility adjusted EBITDA in 2022?" 
 # query = "What is the status of the classification of drivers?"
 # query = "What is the comprehensive income (loss) attributable to Uber reported in 2021?"
 # query = "What is the comprehensive income (loss) attributable to Uber Technologies reported in 2022?"
+query = "What are the data shown in the bar graph titled 'Monthly Active Platform Consumers'?"
+# query = "Can you tell me the page number on which the bar graph titled 'Monthly Active Platform Consumers' is located?"
+# query = "What are the data shown in the bar graph titled 'Monthly Active Platform Consumers' on page 43?"
+# query = "What is the Q2 2020 value shown in the bar graph titled 'Monthly Active Platform Consumers' on page 43?"
+# query = "What are the main risk factors for Uber?"
+# query = "What are the data shown in the bar graph titled 'Trips'?"
+# query = "What are the data shown in the bar graph titled 'Gross Bookings'?"
+
 
 response = recursive_query_engine.query(query)
 
